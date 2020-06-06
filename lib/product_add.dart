d9b7f58e309abb8a00083b70d3a133646a00905f
kaju02525 <karunkumar02525@gmail.com>
2020-06-07 00:32:37 +0530

    add photo frame

--- lib/product_add.dart
+++ lib/product_add.dart
@@ -1,9 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:popwoot/ui/theme.dart';
-import 'package:popwoot/ui/widgets/button_widget.dart';
 import 'package:popwoot/ui/widgets/text_widget.dart';
 import 'package:popwoot/ui/widgets/textfield_widget.dart';
-
 
 class ProductAdds extends StatefulWidget {
   @override
@@ -13,55 +13,94 @@ class ProductAdds extends StatefulWidget {
 class _ProductAddsState extends State<ProductAdds> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        titleSpacing: 2.0,
-        title: Text(
-          "Add Product",
-          style: TextStyle(
-            letterSpacing: 1.0,
-            fontSize: 22.0,
-            fontWeight: FontWeight.w700,
-            fontFamily: font,
+    return Scaffold(
+      backgroundColor: Colors.white,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: Theme.of(context).primaryColor,
+          onPressed: () {},
+          isExtended: true,
+          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          icon: Icon(Icons.playlist_add),
+          label: Text('Add Category'),
+        ),
+        appBar: AppBar(
+          titleSpacing: 2.0,
+          title: Text(
+            "Add Product",
+            style: TextStyle(
+              letterSpacing: 1.0,
+              fontSize: 22.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: font,
+            ),
           ),
         ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            getAddImage(),
+            getEditBox(),
+          ],
+        )));
+  }
+
+  Widget getAddImage() {
+    return Container(
+      width: double.infinity,
+      height: 150.0,
+      child: Center(
+        child: RaisedButton.icon(
+          onPressed: () {
+            print('Button Clicked.');
+          },
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          label: Text(
+            'Add Photos',
+            style: TextStyle(color: Colors.white),
+          ),
+          icon: Icon(
+            Icons.camera_alt,
+            color: Colors.white,
+          ),
+          textColor: Colors.white,
+          splashColor: Colors.red,
+          color: Colors.lightBlue,
+        ),
       ),
-     body:  Container(
-         child: getEditBox(),
-     ));
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors:[Colors.deepOrange[300],Colors.white],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )
+      )
+    );
   }
 
-
-
-  Widget getEditBox(){
-       return Padding(
-         padding: const EdgeInsets.only(top:8.0,left: 10.0,right: 10.0),
-         child: Column(
-             mainAxisAlignment: MainAxisAlignment.start,
-             crossAxisAlignment: CrossAxisAlignment.start,
-             children: <Widget>[
-
-                 TextWidget('Category Name'),
-                 TextFieldWidget(
-                   hintText: 'Enter category name',
-                 ),
-
-               TextWidget('Category Description'),
-               TextFieldWidget(
-                 minLine: 4,
-                 hintText: 'Enter category description',
-               ),
-
-                TextWidget('Category urls'),
-                TextFieldWidget(
-                 hintText: 'Enter category urls',
-               ),
-               ButtonWidget(
-                 title:'Add Category',)
-
-             ],
-         ),
-       );
+  Widget getEditBox() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, left: 10.0, right: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextWidget('Category Name'),
+          TextFieldWidget(
+            hintText: 'Enter category name',
+          ),
+          TextWidget('Category Description'),
+          TextFieldWidget(
+            minLine: 3,
+            hintText: 'Enter category description',
+          ),
+          TextWidget('Category urls'),
+          TextFieldWidget(
+            hintText: 'Enter category urls',
+          ),
+        ],
+      ),
+    );
   }
-
 }
